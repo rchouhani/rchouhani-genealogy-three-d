afe6a69fefeebd89e8339243f2952acbeaab7826
rchouhani <rchouhani@me.com>
2025-10-27 22:16:55 +0100

    ADD : functions buttons

---
+++ app/components/ControlsPanel.tsx
@@ -0,0 +1,24 @@
+export default function ControlsPanel({ onZoomIn, onZoomOut, onReset }: any) {
+  return (
+    <div className="fixed bottom-6 right-6 flex flex-col gap-2">
+      <button
+        onClick={onZoomIn}
+        className="bg-blue-500 text-white px-3 py-2 rounded-md"
+      >
+        +
+      </button>
+      <button
+        onClick={onZoomOut}
+        className="bg-red-500 text-white px3 py-2 rounded-md"
+      >
+        -
+      </button>
+      <button
+        onClick={onReset}
+        className="bg-red-500 text-white px3 py-2 rounded-md"
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
