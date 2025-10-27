ca8c2f4bc0bca9fdaedaace7e9afc41dcc38030e
rchouhani <rchouhani@me.com>
2025-10-27 22:17:45 +0100

    ADD : Line interface

--- app/types/family.tsx
+++ app/types/family.tsx
@@ -1,11 +1,20 @@
+import * as THREE from "three";
+
 export interface Relation {
-    id: number;
-    type: "child" | "parent" | "sibling";
+  id: number;
+  type: "child" | "parent" | "sibling";
 }
 
 export interface Person {
-    id: number;
-    name: string;
-    generation: number;
-    relations: Relation[];
+  id: number;
+  name: string;
+  generation: number;
+  relations: Relation[];
+}
+
+export interface Line {
+  parent: number;
+  child: number;
+  type: string;
+  line: THREE.Line;
 }
