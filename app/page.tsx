8c110c1f53d70b6b14abbdf33ac306bd9f6f7f87
rchouhani <rchouhani@me.com>
2025-11-02 20:31:33 +0100

    ADD : controlpanels

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import ControlsPanel from "./components/ControlsPanel";
 import TreeScene from "./components/TreeScene";
 
 export default function Home() {
   return (
     <main className="relative">
       <TreeScene />
+      <ControlsPanel />
     </main>
   );
 }
